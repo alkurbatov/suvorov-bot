87fa3dc52c17fb19f7bb3251938d55210189d78b
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2017-11-19 11:09:58 +0300

    travis: Drop redundant semicolons

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test; # For C++14
-    sudo apt-get -qq update;
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # For C++14
+    sudo apt-get -qq update
 fi
--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sudo apt-get install -y g++-5;
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 90;
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90;
+    sudo apt-get install -y g++-5
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 90
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
 fi
