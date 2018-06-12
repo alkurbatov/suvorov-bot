2ecccb9e382d7ce87e114f9e6f021a9f0105b64b
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2018-06-12 15:47:43 +0300

    .travis: Workaround broken builds
    I am tired to wait till my PR will be merged into Blizzard's civetweb
    fork. Lets apply a simple workaround for the Travis builds.

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,6 +8,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt-get install -y g++-5
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 90
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
+else
+    # Apply compilation fixes for OS X.
+    git apply hacks/civetweb_compilation_fix.patch
 fi
 
 curl -o cpplint.py https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
