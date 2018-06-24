bce63312e3585a4b272911063866ae262d27bb18
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2018-06-24 11:56:31 +0300

    Log in the same folder where we are ran
    I am not sure should we log something in release mode but definitely all
    the files should be placed in the same folder where the bot runs.

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    gHistory.Init("bin/history.log");
+    gHistory.Init("history.log");
 
     sc2::Coordinator coordinator;
     coordinator.LoadSettings(1, argv);
