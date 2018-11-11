6424c76c0d578a0353421f4deb02d57232868c2f
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2018-11-11 19:51:50 +0300

    core: Change timers threshold values
    Set the treshold values according to the ladder manager limits and generate
    proper log message when needed. To high step processing time could lead
    to ban.

--- src/core/Timer.cpp
+++ src/core/Timer.cpp
@@ -13,8 +13,17 @@ void Timer::Finish() {
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
         std::chrono::high_resolution_clock::now() - m_start).count();
 
-    if (duration < 100000)
+    // NOTE (alkurbatov): 60ms is the maximum step processing time allowed
+    // by the ladder. Higher values could lead to ban.
+    if (duration > 60000) {
+        gHistory.error() << "Step processing took: " << duration << " mcs" << std::endl;
         return;
+    }
 
-    gHistory.warning() << "Time consumed: " << duration << " microseconds" << std::endl;
+    // NOTE (alkurbatov): The ladder allows up to 44.4ms for step processing.
+    // Ideally, we shouldn't reach this threshold.
+    if (duration > 44400) {
+        gHistory.warning() << "Step processing took: " << duration << " mcs" << std::endl;
+        return;
+    }
 }
