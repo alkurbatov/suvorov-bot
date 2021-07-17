b107cc205cc867883e3c22001806eacc089d92fe
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-07-17 13:18:02 +0300

    chore: Replace VSCode with Emacs
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode . ((flycheck-cppcheck-standards . "c++14")
+	      (flycheck-clang-language-standard . "c++14"))))
