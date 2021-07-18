458c17c9dee80cceb7ff1f5b32acd6360006501d
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-07-18 16:02:57 +0300

    chore: Expand Emacs settings
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,13 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c++-mode . ((flycheck-cppcheck-standards . "c++14")
-	      (flycheck-clang-language-standard . "c++14"))))
+((nil
+  (eval . (let ((root (projectile-project-root)))
+            (setq-local flycheck-cppcheck-standards "c++14")
+            (setq-local flycheck-clang-language-standard "c++14")
+            (setq-local flycheck-clang-include-path
+                        (list (concat root "src")
+                              (concat root "contrib/cpp-sc2/include")
+                              (concat root "contrib/cpp-sc2/contrib/protobuf/src")
+                              (concat root "contrib/cpp-sc2/generated")
+                              (concat root "build/contrib/cpp-sc2/generated")))))))
