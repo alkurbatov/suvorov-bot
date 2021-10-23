a640a0b289e015070be3e033ac8fcb124ff5e8f1
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-10-23 13:16:00 +0300

    chore: Refine local Emacs config
    * Drop settings which were moved to default values of my configs.
    * Add gcc settings.
    * Better syntax.
    
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,15 +1,14 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (eval . (let ((root (projectile-project-root)))
-            (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-            (setq-local flycheck-cppcheck-standards "c++14")
-            (setq-local flycheck-clang-language-standard "c++14")
-            (setq-local flycheck-clang-include-path
-                        (list (concat root "build")
-                              (concat root "src")
-                              (concat root "contrib/cpp-sc2/include")
-                              (concat root "contrib/cpp-sc2/contrib/protobuf/src")
-                              (concat root "contrib/cpp-sc2/generated")
-                              (concat root "build/contrib/cpp-sc2/generated")))))))
+((c++-mode
+  (flycheck-cppcheck-standards      . "c++14")
+  (flycheck-clang-language-standard . "c++14")
+  (flycheck-gcc-language-standard   . "c++14")
+  (eval progn (let
+                  ((local-include-paths
+                    (list (concat (projectile-project-root) "build")
+                          (concat (projectile-project-root) "src")
+                          (concat (projectile-project-root) "contrib/cpp-sc2/include")
+                          (concat (projectile-project-root) "contrib/cpp-sc2/contrib/protobuf/src")
+                          (concat (projectile-project-root) "contrib/cpp-sc2/generated")
+                          (concat (projectile-project-root) "build/contrib/cpp-sc2/generated"))))
+                (setq flycheck-clang-include-path local-include-paths)
+                (setq flycheck-gcc-include-path local-include-paths)))))
